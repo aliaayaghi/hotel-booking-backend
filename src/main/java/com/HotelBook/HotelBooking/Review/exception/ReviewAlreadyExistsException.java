d34e997d47c09bf27d14bffbd819690762391f31
Aliaa <aliaarami235@gmail.com>
2026-04-04 18:18:55 +0300

    bug: fixed review already exists exception

--- src/main/java/com/HotelBook/HotelBooking/Review/exception/ReviewAlreadyExistsException.java
+++ src/main/java/com/HotelBook/HotelBooking/Review/exception/ReviewAlreadyExistsException.java
@@ -4,7 +4,7 @@ package com.HotelBook.HotelBooking.Review.exception;
 import java.util.UUID;
 
 public class ReviewAlreadyExistsException extends RuntimeException {
-    public ReviewAlreadyExistsException(UUID bookingId) {
+    public ReviewAlreadyExistsException(UUID bookingId)  {
         super("A review already exists for booking ID: " + bookingId);
     }
 }
